efab9a0bb9182e061a40b4b92111a577cf245201
Rozhnov Alexandr <nox73@ya.ru>
2014-12-25 17:31:13 +0300

    Init

---
+++ terraform.tf
@@ -0,0 +1,28 @@
+# Configure the DigitalOcean Provider
+
+variable "do_token" {}
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "demo-entrypoint" {
+    image = "coreos-alpha"
+    name = "demo-entrypoint"
+    region = "ams3"
+    size = "1gb"
+    private_networking = "true"
+    user_data = "${file("user-data.entrypoint")}"
+    ssh_keys = [581252]
+}
+
+resource "digitalocean_droplet" "demo-node" {
+    count = 2
+    image = "coreos-alpha"
+    name = "demo-node"
+    region = "ams3"
+    size = "1gb"
+    private_networking = "true"
+    user_data = "${file("user-data.node")}"
+    ssh_keys = [581252]
+}
