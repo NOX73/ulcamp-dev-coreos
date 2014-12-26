2b1a8fac286077aa4edcd9edfa139f09094f11c5
Rozhnov Alexandr <nox73@ya.ru>
2014-12-26 13:30:31 +0300

    Switch postgres to redis

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node:0.10.34
 
-ADD ./app /usr/src/app
+WORKDIR /usr/src/app
 
-CMD ["node", "/usr/src/app/index.js"]
+ADD ./app /usr/src/app
+RUN npm install
+
+CMD ["node", "index.js"]
--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var redis = require("redis");
+
+var redisHost = process.env.REDIS_HOST_PORT.split(":");
+var c = redis.createClient(redisHost[1], redisHost[0])
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("My ip: " + process.env.COREOS_PRIVATE_IPV4);
+  c.incr("requests", function(err, total) {
+    response.end("My ip: " + process.env.COREOS_PRIVATE_IPV4 + ".\n Total requests: " + total);
+  })
 });
 
 server.listen(3000);
